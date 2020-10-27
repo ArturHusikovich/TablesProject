2324a9682c770a3b93dbcee3233fd260ffd0c36e
Artur Husikovich <fda.husikovich@gmail.com>
2020-10-27 15:44:21 +0400

    Changes preview component

--- src/components/MainContent/Preview/Preview.js
+++ src/components/MainContent/Preview/Preview.js
@@ -3,8 +3,20 @@ import styles from './Preview.module.css';
 
 const Preview = (props) => {
     return (
-    <div className={styles.item}>
-        <div className={styles.sec}>{props.user.name}</div>
+    <div>
+
+        <div className={styles.item}>
+            <div className={styles.box}>
+                {/* <img src={ props.user.image === 'cat' ? cat : pig } alt="Avatar" className={styles.ava} /> */}
+            </div>
+            <div className={styles.dbox}>{props.user.name}</div>
+            <div className={styles.box}>{props.user.age}</div>
+            <div className={styles.dbox}>{props.user.phone}</div>
+            <div className={styles.box}>
+                {/* <img src={ !props.user.favourite ? follow : unfollow} alt="Star" className={styles.starImg} /> */}
+            </div>
+
+        </div>
  
 
     </div>
